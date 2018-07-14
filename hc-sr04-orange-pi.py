2907c81911fb1ee7aaf4139dcc6eb5900e3c2eff
ionutpi <mltrionut@yahoo.co.uk>
2018-07-14 09:23:06 +0200

    added comments

--- hc-sr04-orange-pi.py
+++ hc-sr04-orange-pi.py
@@ -3,31 +3,34 @@ from pyA20.gpio import port
 from time import sleep
 import time
 
-
-
-
 #initialize the gpio module
 gpio.init()
 
-#setup the port (same as raspberry pi's gpio.setup() function)
+#setup the ports
 gpio.setcfg(port.PC4, gpio.OUTPUT)
 gpio.setcfg(port.PC7, gpio.INPUT)
 
+#
 gpio.output(port.PC4, gpio.LOW)
 sleep(0.5)
 
 while True:
+    #send pulse
     gpio.output(port.PC4, gpio.HIGH)
     sleep(0.3)
     gpio.output(port.PC4, gpio.LOW)
-    #state=gpio.input(port.PC7)
+
+    #wait for echo to start
     while (gpio.input(port.PC7) == 0):
         pass
     start = time.time()
+
+    #wait for echo to end
     while (gpio.input(port.PC7) == 1):
         pass
     end = time.time()
-
+    
+    #get distance in cm
     print((end - start)/58.0*1000000)
     sleep(1)
-#print state
+
